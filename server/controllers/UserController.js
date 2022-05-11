ce5d4ccf17f1e29abf446b73f064857e55087607
xjoninha <jonasjfs15@gmail.com>
2022-05-11 19:37:42 -0300

    feat: reajustando mudanças no UserControllers

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -1,7 +1,7 @@
 const users = require('../data/users')
 
 const controller = {
-    index: (req, res) => res.render('users', {title: "Usuários", users})
+  index: (req, res) => res.render('users', { title: "Usuários", users })
 }
-  
+
 module.exports = controller
