0129c07b48a220c40b08e444303497464be28b84
xjoninha <jonasjfs15@gmail.com>
2022-05-11 20:08:47 -0300

    feat: adicionando mais funções no controller e algumas rotas nos users

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -1,7 +1,18 @@
 const users = require('../data/users')
 
 const controller = {
-  index: (req, res) => res.render('users', { title: "Usuários", users })
+  index: (req, res) => res.render('users', { title: "Usuários", users: users }),
+  show: (req, res) => {
+    console.log(req.params.id)
+    const foundUser = users.filter(user => user.id == req.params.id)
+    if(!foundUser.length) {
+      res.render('users', { title: "Usuário Não Encontrado", users: null })
+    }
+    res.render('users', { title: "Usuário por ID", users: foundUser })
+  },
+  assinante: (req, res) => {
+    res.render('users', { title: "Usuários ativos", users: users.filter(user => user.assinante) })
+  }
 }
 
 module.exports = controller
--- server/routes/users.js
+++ server/routes/users.js
@@ -4,5 +4,7 @@ const router = express.Router();
 
 /* GET users listing. */
 router.get('/', controller.index);
+router.get('/assinante', controller.assinante);
+router.get('/:id', controller.show);
 
 module.exports = router;
