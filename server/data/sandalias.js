84211b6461555494f41f7eacb83abdd3cbca350d
xjoninha <jonasjfs15@gmail.com>
2022-05-13 15:18:35 -0300

    feat: criando arquivo sandalias.js na pasta data

---
+++ server/data/sandalias.js
@@ -0,0 +1,32 @@
+const sandalias = [
+    {
+      id: 1,
+      marca: "Marisa",
+      cor: "#0f0",
+      ano: 1998,
+      exclusiva: true
+    },
+    {
+      id: 2,
+      marca: "Arezo",
+      cor: "#00f",
+      ano: 2002,
+      exclusiva: true
+    },
+    {
+      id: 3,
+      marca: "Melissa",
+      cor: "#f0f",
+      ano: 1998,
+      exclusiva: false
+    },
+    {
+      id: 4,
+      marca: "Dolce&Gabana",
+      cor: "#ff0",
+      ano: 2020,
+      exclusiva: false
+    }
+  ]
+  
+  module.exports = sandalias
